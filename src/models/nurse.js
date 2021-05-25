18743a1d6cd8e4f8a063c71a897fd6a8339ca08f
Jose Matias <josematias92@gmail.com>
2021-05-25 00:32:10 -0500

    back to normal

--- src/models/nurse.js
+++ src/models/nurse.js
@@ -14,7 +14,6 @@ const NurseSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
-    patients: [],
     tank: Object
   })
 const Nurse = mongoose.model('Nurse', NurseSchema)
