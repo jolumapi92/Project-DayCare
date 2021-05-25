d752c7738e41c91184f013fd72438b853e0a40ab
Jose Matias <josematias92@gmail.com>
2021-05-24 21:31:42 -0500

    add

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const path = require("path");
+const mongoose = require('mongoose');
 
 const app = express()
 const port = process.env.PORT || 5000
